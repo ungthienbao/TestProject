86aaf876d23d10a89405a14dee0f7b7258ad8285
Danh Dat <thienbao.important@gmail.com>
2016-12-24 22:58:16 +0700

    add roller ball character

---
+++ Assets/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Vehicles.Ball;
+public class CameraController : MonoBehaviour {
+    public Ball ball;
+    private Vector3 offset;
+	// Use this for initialization
+	void Start () {
+        ball = GameObject.FindObjectOfType<Ball>();
+        offset = this.transform.position - ball.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = ball.transform.position + offset;
+	}
+}
